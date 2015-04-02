24f5943ddc1bb40615c0577107b7ef9d35a9ceb1
Alexander Færøy <ahf@0x90.dk>
2015-04-02 17:02:27 +0200

    Add utilities for working with hex encoded data.

--- apps/celo_core/src/celo_core_utilities.erl
+++ apps/celo_core/src/celo_core_utilities.erl
@@ -12,7 +12,9 @@
 
 %% API.
 -export([expand_tilde/1,
-         home_dir/0
+         home_dir/0,
+         hex_to_binary/1,
+         binary_to_hex/1
         ]).
 
 -spec expand_tilde(Path :: file:filename()) -> file:filename().
@@ -28,3 +30,27 @@ home_dir() ->
         Homedir ->
             Homedir
     end.
+
+%% @doc Convert hex to binary.
+-spec hex_to_binary(Data :: binary()) -> binary();
+                   (Data :: string()) -> binary().
+hex_to_binary([A, B | Rest]) ->
+    <<(list_to_integer([A, B], 16)), (hex_to_binary(Rest))/binary>>;
+
+hex_to_binary([A]) ->
+    <<(list_to_integer([A], 16))>>;
+
+hex_to_binary([]) ->
+    <<>>;
+
+hex_to_binary(Data) when is_binary(Data) ->
+    hex_to_binary(binary_to_list(Data)).
+
+%% @doc Convert binary data to hex.
+-spec binary_to_hex(Data :: binary()) -> binary();
+                   (Data :: string()) -> binary().
+binary_to_hex(Data) when is_binary(Data) ->
+    list_to_binary(lists:flatten([integer_to_list(X, 16) || <<X:4/integer>> <= Data]));
+
+binary_to_hex(Data) when is_list(Data) ->
+    binary_to_hex(list_to_binary(Data)).
